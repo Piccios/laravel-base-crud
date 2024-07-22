fd8b190b0cb30d2d87b19e9162f5520b1e742d51
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-22 18:24:32 +0200

    created a seeder

---
+++ database/seeders/AnimalSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class AnimalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    //per n volte
+        //prendo il modello
+        //lo popolo
+        //lo salvo
+
+
+
+    }
+}
