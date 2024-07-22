c80fbd42cf5f8cb24434511df2c8b07cb3cb1385
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-22 18:42:27 +0200

    populated seeder with Faker

--- database/seeders/AnimalSeeder.php
+++ database/seeders/AnimalSeeder.php
@@ -2,17 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Animal;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 
 class AnimalSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
+        for($i=0; $i<50; $i++){
+            $animal = new Animal();
+            $animal->nome = $faker->name();
+            $animal->specie = $faker->word();
+            $animal->razza = $faker->word();
+            $animal->eta = $faker->numberBetween(1, 10);
+            $animal->peso = $faker->randomFloat(2, 1, 10);
+            $animal->sesso = $faker->numberBetween(1, 2);
+
+        }
     //per n volte
         //prendo il modello
         //lo popolo
