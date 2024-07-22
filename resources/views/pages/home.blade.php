53e3946e08d9dbee3e9abd29745130a7ffbe5f94
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-22 20:27:24 +0200

    animal cards shown on page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,37 @@
 
     @section('main-content')
 
-    <div>main</div>
+    <section class="header">
+        <div class="container">
+            <h1>I nostri amici animali</h1>
+        </div>
+        <section class="row row-cols-1 row-cols-md-3 g-4 p-2">
+            @foreach ($animals as $animal)
+            <article class="col animal-item mb-3">
+                <div class="card text-center">
+                    <img src="{{$animal->url_img}}" class="card-img-top" alt="animalIMG">
+
+                    <div class="card-body">
+                        <h3 class="card-title">Specie: {{ $animal->specie }}</h3>
+                        <h3 class="card-title">Nome: {{ $animal->nome }}</h3>
+                        <p class="card-text">
+                            {{ $animal->info }}
+                        </p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Età: {{ $animal->eta}} anni</li>
+                        <li class="list-group-item">Peso: {{ $animal->peso}} kg</li>
+                        <li class="list-group-item">Altezza: {{ $animal->altezza}} cm</li>
+                    </ul>
+                    <div class="card-body">
+                    </div>
+                </div>
+            </article>
+
+            @endforeach
+
+        </section>
+
+    </section>
 
     @endsection
