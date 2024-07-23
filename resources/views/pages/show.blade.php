3982daf8f196a47b4b6dc967d957f749f1565c91
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-23 08:53:24 +0200

    created a page show to provide a single animal view

---
+++ resources/views/pages/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.app')
+@section('page-title', 'Homepage')
+@section('main-content')
+
+
+    <div class="card" style="width: 18rem;">
+        <img src="{{ $animal->url_img }}" class="card-img-top" alt="animal img">
+
+        <div class="card-body">
+            <h5 class="card-title">Nome: {{ $animal->nome }}</h5>
+            <h3 class="card-title">Specie: {{ $animal->specie }}</h3>
+            <p class="card-text">Info: {{ $animal->info }}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Eta': {{ $animal->eta }}</li>
+            <li class="list-group-item">Peso: {{ $animal->peso }}</li>
+            <li class="list-group-item">Sesso:
+                @if ($animal->sesso == 1)
+                    Maschio
+                @elseif ($animal->sesso == 2)
+                    Femmina
+                @else
+                    Non specificato
+                @endif
+            </li>
+        </ul>
+        <div class="card-body">
+            <a href="{{ route('pages.index') }}" class="card-link d-flex justify-content-center">Gli altri animali</a>
+
+        </div>
+    </div>
+
+@endsection
