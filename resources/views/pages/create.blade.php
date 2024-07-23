57e3eaddb460ea2ebff181f171a558e19888574e
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-23 15:16:24 +0200

    set a create page

---
+++ resources/views/pages/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('page-title')
+    Add a new animal to the family
+@section('main-content')
+
+<section class="container d-flex justify-content-center mt-5">
+    <div class="card" style="width: 18rem;">
+
+    </div>
+</section>
+
+@endsection
