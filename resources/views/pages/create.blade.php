52774350fe30c00e81af280bc7deb4338e239986
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-23 15:54:47 +0200

    set a form

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -2,14 +2,55 @@
 @section('page-title', 'Add a new animal')
 @section('main-content')
 
-<section class="container d-flex justify-content-center mt-5">
-    <div class="row justify-content-center">
+    <section class="container text-center mt-5">
+        <div class="row justify-content-center">
 
-        <h1 class="mb-3 fw-bolder text-center">
-            Add a new animal to the family
-        </h1>
+            <h1 class="mb-3 fw-bolder text-center">
+                Aggiungi un nuovo animale
+            </h1>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-8">
+                <form action="{{ route('pages.store') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="nome">Nome</label>
+                        <input class="form-control form-control-lg" type="text" placeholder="nome animale"
+                            aria-label="nome animale" id="nome" name="nome">
+                    </div>
+                    <div class="mb-3">
+                        <label for="specie" class="form-label">Specie</label>
+                        <input class="form-control form-control-lg" type="text" id="specie" name="specie" placeholder="Specie dell'animale">
+                    </div>
+                    <div class="mb-3">
+                        <label for="eta" class="form-label">Età</label>
+                        <input class="form-control form-control-lg" type="number" id="eta" name="eta" placeholder="Età dell'animale">
+                    </div>
+                    <div class="mb-3">
+                        <label for="peso" class="form-label">Peso</label>
+                        <input class="form-control form-control-lg" type="number" step="0.01" id="peso" name="peso" placeholder="Peso dell'animale in kg">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sesso" class="form-label">Sesso</label>
+                        <select class="form-control form-control-lg" id="sesso" name="sesso">
+                            <option value="1">Maschio</option>
+                            <option value="0">Femmina</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="url_img" class="form-label">Immagine</label>
+                        <input class="form-control form-control-lg" type="file" id="url_img" name="url_img">
+                    </div>
+                    <div class="mb-3">
+                        <label for="note" class="form-label">Note</label>
+                        <textarea class="form-control form-control-lg" id="note" name="note" placeholder="Note sull'animale" rows="5"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-lg">Aggiungi Animale</button>
+            </div>
 
-    </div>
-</section>
+
+            </form>
+        </div>
+    </section>
 
 @endsection
